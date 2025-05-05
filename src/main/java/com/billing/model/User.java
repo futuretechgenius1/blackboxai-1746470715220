59b3760b603e18578e3b529762f045a1dc407304
futuretechgenius1 <manoharyadavit@gmail.com>
2025-05-06 00:16:22 +0530

    Add src/main/java/com/billing/model/User.java 1746470781183

---
+++ src/main/java/com/billing/model/User.java
@@ -0,0 +1,34 @@
+package com.billing.model;
+
+import lombok.Data;
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(nullable = false)
+    private String name;
+    
+    @Column(nullable = false, unique = true)
+    private String email;
+    
+    @Column(nullable = false)
+    private String password;
+    
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+        name = "user_roles",
+        joinColumns = @JoinColumn(name = "user_id"),
+        inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles = new HashSet<>();
+    
+    private boolean enabled = true;
+}
